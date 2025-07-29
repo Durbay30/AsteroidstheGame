5847622e00f365c36d916f061aafdfd1b3958259
durbay30 <durbay30@icloud.com>
2025-07-28 20:15:10 -0400

    movement of shit added

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(SCREEN_WIDTH /2 , SCREEN_HEIGHT / 2)
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+  
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return    
+        screen.fill("black")
+        dt = clock.tick(60) / 1000
+        player1.update(dt)
+        player1.draw(screen)
         pygame.display.flip()
-            
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+    
 
 
 if __name__ == "__main__":
